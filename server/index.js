d7e3b62e750a6a6487ec0eaa6cc5dd7efa0692b1
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-04-15 15:08:10 +0200

    Added SystemMessage to chat history

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.post('/question', async (req, res) => {
-    const {system, messages} = req.body;
+    const { system, messages } = req.body;
 
     try {
         const formattedMessages = [
